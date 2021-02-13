f404f1e66587800c5f8a50ada57732066c1dde86
Chris Crawford <christopher.crawford1@outlook.com>
2021-02-13 10:42:38 +0000

    - Some responsive work, hide the table on mobile devices for now. Will fix in future.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,11 +6,11 @@
 @section('content')
     <div class="container mx-auto mt-8">
             <h1 class="md:text-5xl text-3xl text-center text-stone-500 mb-8">VatFlights</h1>
-            <p class="text-center text-stone-500">
+            <p class="text-center text-stone-500 ml-3 mr-3">
                 A place to get current and previous statistics about flights taking place on the
                 <a href="https://www.vatsim.net/" class="hover:underline text-blue-500">Vatsim Network</a>.
             </p>
-        <p class="text-center text-stone-500 mt-2">
+        <p class="text-center text-stone-500 mt-2 ml-3 mr-3">
             For more information about this project, check out the About page.
         </p>
 
